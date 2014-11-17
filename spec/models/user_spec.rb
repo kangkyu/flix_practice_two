7724f51da765e4c49fdff0acdbb5480f535e3c0d
kangkyu <kangkyu1111@gmail.com>
2014-11-17 03:18:43 -0800

    refactor login authentication

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -69,3 +69,19 @@ describe 'A user' do
   end
 
 end
+
+describe "authenticate" do
+  before do
+    @user = User.create!(user_attributes)
+  end
+  it "returns non-true value if the email does not match" do
+    expect(User.authenticate("nomatch-email",@user.password)).not_to eq(true)
+  end
+  it "returns non-true value if the password does not match" do
+    expect(User.authenticate(@user.email,"nomatch-password")).not_to eq(true)
+  end
+  it "returns the user if the email and password match" do
+    expect(User.authenticate(@user.email,@user.password)).to eq(@user)
+  end
+
+end
